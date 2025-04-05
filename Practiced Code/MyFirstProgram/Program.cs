698243b0f4641b88060ad4fd809e275e8b111c53
Manish Royan <manishroyanofficial@outlook.com>
2025-04-05 21:38:33 +0545

    A simple 'Hello World' program.

---
+++ Practiced Code/MyFirstProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("2025/04/05");
+            Console.Beep(); //the program will going to beep when it reaches the end
+        }
+    }
+}
